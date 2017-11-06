bb553fd46b2ea55585d25d4da6599248fe99092b
Jason Graham <jgraham@compukix.net>
2017-11-05 21:24:59 -0500

    Added elem_dtor to the queue and stack data structures which is initialized upon creation.
    Updated the shared object versioning to reflect non-backward compatible API changes.

--- queue/queue.c
+++ queue/queue.c
@@ -5,181 +5,185 @@
  */
 
 #include <assert.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <libbds/bds_queue.h>
 #include "memutil.h"
+#include <libbds/bds_queue.h>
 
 /* static inline double log2( double x ) */
 /* { */
 /* 	return ( log(x) / log(2.0) ); */
 /* } */
 
-inline static void __queue_incr_front( struct bds_queue *queue )
+inline static void __queue_incr_front(struct bds_queue *queue)
 {
-	queue->front = BDS_MOD( queue->front + 1, queue->n_alloc);
+        queue->front = BDS_MOD(queue->front + 1, queue->n_alloc);
 }
 
-inline static size_t __queue_end( const struct bds_queue *queue )
+inline static size_t __queue_end(const struct bds_queue *queue)
 {
-	assert( queue->n_alloc > 0 );
-	return BDS_MOD( bds_queue_back( queue ) + 1, queue->n_alloc );
+        assert(queue->n_alloc > 0);
+        return BDS_MOD(bds_queue_back(queue) + 1, queue->n_alloc);
 }
 
-inline static const void *__queue_endptr( const struct bds_queue *queue )
+inline static const void *__queue_endptr(const struct bds_queue *queue)
 {
-	return (const void *)( queue->v + __queue_end( queue ) * queue->elem_len );
+        return (const void *)(queue->v + __queue_end(queue) * queue->elem_len);
 }
 
-
-void bds_queue_ctor( struct bds_queue *queue, size_t n_alloc, size_t elem_len )
+void bds_queue_ctor(struct bds_queue *queue, size_t n_alloc, size_t elem_len, void (*elem_dtor)(void *))
 {
-	memset(queue, 0, sizeof(*queue));
+        memset(queue, 0, sizeof(*queue));
 
 #ifdef BDS_USE_OPTIMIZED_MOD
-	queue->n_alloc   = (1UL << (unsigned long)ceil( log2( n_alloc ) ));
+        queue->n_alloc = (1UL << (unsigned long)ceil(log2(n_alloc)));
 #else
-	queue->n_alloc   = n_alloc;
+        queue->n_alloc = n_alloc;
 #endif
-	queue->elem_len = elem_len;
+        queue->elem_len = elem_len;
 
-	queue->v = xalloc( n_alloc * elem_len );
+        queue->v         = xalloc(n_alloc * elem_len);
+        queue->elem_dtor = elem_dtor;
 }
 
-void bds_queue_dtor( struct bds_queue *queue, void (*elem_dtor)(void *) )
+void bds_queue_dtor(struct bds_queue *queue)
 {
-	if( queue->v != NULL ) {
-		bds_queue_clear( queue, elem_dtor );
-		free( queue->v );
-	}
-	memset( queue, 0, sizeof(*queue) );
+        if (queue->v != NULL) {
+                bds_queue_clear(queue);
+                free(queue->v);
+        }
+        memset(queue, 0, sizeof(*queue));
 }
 
-struct bds_queue *bds_queue_alloc( size_t n_alloc, size_t elem_len )
+struct bds_queue *bds_queue_alloc(size_t n_alloc, size_t elem_len, void (*elem_dtor)(void *))
 {
-	struct bds_queue *queue = xalloc( sizeof(*queue) );
-	bds_queue_ctor( queue, n_alloc, elem_len );
-	return queue;
+        struct bds_queue *queue = xalloc(sizeof(*queue));
+        bds_queue_ctor(queue, n_alloc, elem_len, elem_dtor);
+        return queue;
 }
 
-void bds_queue_free( struct bds_queue **queue, void (*elem_dtor)(void *) )
+void bds_queue_free(struct bds_queue **queue)
 {
-	if( *queue == NULL ) return;
+        if (*queue == NULL)
+                return;
 
-	bds_queue_dtor( *queue, elem_dtor );
-	free( *queue );
-	*queue = NULL;
+        bds_queue_dtor(*queue);
+        free(*queue);
+        *queue = NULL;
 }
 
-void bds_queue_resize( struct bds_queue *queue )
+void bds_queue_resize(struct bds_queue *queue)
 {
-	const size_t n_alloc   = queue->n_alloc << 1; // Double the size (need overflow check here)
-	const size_t elem_len = queue->elem_len;
-	
-	queue->v = xrealloc( queue->v, queue->n_alloc * elem_len, n_alloc * elem_len );
+        const size_t n_alloc  = queue->n_alloc << 1; // Double the size (need overflow check here)
+        const size_t elem_len = queue->elem_len;
 
-	bds_queue_linearize( queue );
-	queue->n_alloc  = n_alloc;
+        queue->v = xrealloc(queue->v, queue->n_alloc * elem_len, n_alloc * elem_len);
+
+        bds_queue_linearize(queue);
+        queue->n_alloc = n_alloc;
 }
 
-void bds_queue_push( struct bds_queue *queue, const void *v )
-{	
-	if( bds_queue_isfull( queue ) ) {
-		assert( __queue_end( queue ) == queue->front );
-		if( queue->auto_resize ) {
-			bds_queue_resize( queue );
-		} else {
-			fprintf(stderr,"error: bds_queue_push queue is full and auto_resize = false: cannot perform push\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	
-	// Copy to the vacant location
-	memcpy( (void *)__queue_endptr( queue ), v, queue->elem_len );
-	queue->n_elem++;
-	
-}
-
-void *bds_queue_pop( struct bds_queue *queue, void *v )
+void bds_queue_push(struct bds_queue *queue, const void *v)
 {
-	void *v_front = (void *)bds_queue_frontptr( queue );
-	if( v_front == NULL ) return NULL;
-	
-	if( v != NULL ) memcpy( v, v_front, queue->elem_len );
+        if (bds_queue_isfull(queue)) {
+                assert(__queue_end(queue) == queue->front);
+                if (queue->auto_resize) {
+                        bds_queue_resize(queue);
+                } else {
+                        fprintf(stderr,
+                                "error: bds_queue_push queue is full and auto_resize = false: cannot perform push\n");
+                        exit(EXIT_FAILURE);
+                }
+        }
 
-	__queue_incr_front( queue );
-	queue->n_elem--;
-
-	return v_front;
+        // Copy to the vacant location
+        memcpy((void *)__queue_endptr(queue), v, queue->elem_len);
+        queue->n_elem++;
 }
 
-void bds_queue_clear( struct bds_queue *queue, void (*elem_dtor)(void *) )
+void *bds_queue_pop(struct bds_queue *queue, void *v)
 {
-	if( elem_dtor != NULL ) {
-		while( !bds_queue_isempty( queue ) ) {
-			elem_dtor( bds_queue_pop( queue, NULL ) );
-		}
-	}
-	queue->front=0;
-	queue->n_elem=0;
+        void *v_front = (void *)bds_queue_frontptr(queue);
+        if (v_front == NULL)
+                return NULL;
+
+        if (v != NULL)
+                memcpy(v, v_front, queue->elem_len);
+
+        __queue_incr_front(queue);
+        queue->n_elem--;
+
+        return v_front;
 }
 
-void bds_queue_clear_nfront( struct bds_queue *queue, size_t n_clear, void (*elem_dtor)(void *) )
+void bds_queue_clear(struct bds_queue *queue)
 {
-	if( bds_queue_size(queue) < n_clear ) {
-		printf("warning: bds_queue_clear_nfront: n_clear larger than size of queue\n");
-		bds_queue_clear( queue, elem_dtor );
-		return;
-	}
-
-	if( elem_dtor != NULL ) {
-		size_t n;
-		for( n = 0; n<n_clear; ++n ) {
-			elem_dtor( bds_queue_pop( queue, NULL ) );
-		}
-	} else {
-		queue->front = BDS_MOD( queue->front + n_clear, queue->n_alloc);
-		queue->n_elem -= n_clear;
-	}
+        if (queue->elem_dtor != NULL) {
+                while (!bds_queue_isempty(queue)) {
+                        queue->elem_dtor(bds_queue_pop(queue, NULL));
+                }
+        }
+        queue->front  = 0;
+        queue->n_elem = 0;
 }
 
-void bds_queue_linearize( struct bds_queue *queue )
+void bds_queue_clear_nfront(struct bds_queue *queue, size_t n_clear)
 {
-	
-	const size_t front = queue->front;
-	const size_t back  = bds_queue_back( queue );
+        if (bds_queue_size(queue) < n_clear) {
+                printf("warning: bds_queue_clear_nfront: n_clear larger than size of queue\n");
+                bds_queue_clear(queue);
+                return;
+        }
 
-	if( BDS_QUEUE_ISLINEAR( front, back ) ) return;
-
-	const size_t blk_len = (queue->n_alloc - front) * queue->elem_len;
-	void *buffer = xalloc( blk_len );
-
-	// Store the trailing segment of the data vector into a buffer
-	memcpy( buffer, bds_queue_frontptr(queue), blk_len );
-	// Shift the remaining elements to make room for the front of the data vector
-	memmove( queue->v + blk_len, queue->v, front * queue->elem_len );
-	// Copy the front data vector block to the front
-	memcpy( queue->v, buffer, blk_len );
-	free(buffer);
-
-	queue->front = 0;
+        if (queue->elem_dtor != NULL) {
+                size_t n;
+                for (n = 0; n < n_clear; ++n) {
+                        queue->elem_dtor(bds_queue_pop(queue, NULL));
+                }
+        } else {
+                queue->front = BDS_MOD(queue->front + n_clear, queue->n_alloc);
+                queue->n_elem -= n_clear;
+        }
 }
 
-const void *bds_queue_lsearch( const struct bds_queue *queue, const void *key,
-			       int (*compar)( const void *, const void *) )
+void bds_queue_linearize(struct bds_queue *queue)
 {
-	size_t i,j;
-	const void *v;
 
-	j=queue->front;
-	for( i=0; i<queue->n_elem; ++i ) {
-		v = queue->v + j*queue->elem_len;
-		if( compar( key, v ) == 0 ) return v;
-		j = BDS_MOD( j + 1,  queue->n_alloc );
-	}
-	return NULL;
+        const size_t front = queue->front;
+        const size_t back  = bds_queue_back(queue);
+
+        if (BDS_QUEUE_ISLINEAR(front, back))
+                return;
+
+        const size_t blk_len = (queue->n_alloc - front) * queue->elem_len;
+        void *buffer         = xalloc(blk_len);
+
+        // Store the trailing segment of the data vector into a buffer
+        memcpy(buffer, bds_queue_frontptr(queue), blk_len);
+        // Shift the remaining elements to make room for the front of the data vector
+        memmove(queue->v + blk_len, queue->v, front * queue->elem_len);
+        // Copy the front data vector block to the front
+        memcpy(queue->v, buffer, blk_len);
+        free(buffer);
+
+        queue->front = 0;
+}
+
+const void *bds_queue_lsearch(const struct bds_queue *queue, const void *key, int (*compar)(const void *, const void *))
+{
+        size_t i, j;
+        const void *v;
+
+        j = queue->front;
+        for (i = 0; i < queue->n_elem; ++i) {
+                v = queue->v + j * queue->elem_len;
+                if (compar(key, v) == 0)
+                        return v;
+                j = BDS_MOD(j + 1, queue->n_alloc);
+        }
+        return NULL;
 }
--- stack/stack.c
+++ stack/stack.c
@@ -5,114 +5,116 @@
  */
 
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "memutil.h"
 #include <libbds/bds_stack.h>
 
-
-static void resize_stack( struct bds_stack *stack )
+static void resize_stack(struct bds_stack *stack)
 {
-	const size_t len = ( stack->n_alloc * stack->elem_len );
-	stack->v = xrealloc( stack->v, len, len << 1 );
-	stack->n_alloc <<= 1;
+        const size_t len = (stack->n_alloc * stack->elem_len);
+        stack->v         = xrealloc(stack->v, len, len << 1);
+        stack->n_alloc <<= 1;
 }
 
-void bds_stack_ctor( struct bds_stack *stack, size_t n_alloc, size_t elem_len )
+void bds_stack_ctor(struct bds_stack *stack, size_t n_alloc, size_t elem_len, void (*elem_dtor)(void *))
 {
-	memset( stack, 0, sizeof(*stack) );
-	assert( n_alloc > 0 );
+        memset(stack, 0, sizeof(*stack));
+        assert(n_alloc > 0);
 
-	stack->n_alloc = n_alloc;
-	stack->elem_len = elem_len;
-	stack->v = xalloc( n_alloc * elem_len );
+        stack->n_alloc   = n_alloc;
+        stack->elem_len  = elem_len;
+        stack->v         = xalloc(n_alloc * elem_len);
+        stack->elem_dtor = elem_dtor;
 }
 
-void bds_stack_dtor( struct bds_stack *stack, void (*elem_dtor)(void *) )
+void bds_stack_dtor(struct bds_stack *stack)
 {
-	if( stack->v != NULL ) {
-		bds_stack_clear( stack, elem_dtor );
-		free( stack->v );
-	}
-	memset(stack, 0, sizeof(*stack) );
+        if (stack->v != NULL) {
+                bds_stack_clear(stack);
+                free(stack->v);
+        }
+        memset(stack, 0, sizeof(*stack));
 }
 
-void bds_stack_push( struct bds_stack *stack, const void *v )
+void bds_stack_push(struct bds_stack *stack, const void *v)
 {
-	if( stack->n_elem == stack->n_alloc )
-		resize_stack( stack );
-	
-	stack->n_elem++;
-	memcpy( (void *)bds_stack_topptr(stack), v, stack->elem_len );
+        if (stack->n_elem == stack->n_alloc)
+                resize_stack(stack);
+
+        stack->n_elem++;
+        memcpy((void *)bds_stack_topptr(stack), v, stack->elem_len);
 }
 
-void *bds_stack_pop( struct bds_stack *stack, void *v )
+void *bds_stack_pop(struct bds_stack *stack, void *v)
 {
-	void *v_top = (void *)bds_stack_topptr( stack );
-	if( v_top == NULL ) return NULL;
+        void *v_top = (void *)bds_stack_topptr(stack);
+        if (v_top == NULL)
+                return NULL;
 
-	// Only perform the copy if a buffer is provided; otherwise we
-	// are just poping and discarding
-	if( v != NULL ) 
-		memcpy( v, v_top, stack->elem_len );
+        // Only perform the copy if a buffer is provided; otherwise we
+        // are just poping and discarding
+        if (v != NULL)
+                memcpy(v, v_top, stack->elem_len);
 
-	stack->n_elem--;
+        stack->n_elem--;
 
-	return v_top;
+        return v_top;
 }
 
-void bds_stack_clear( struct bds_stack *stack, void (*elem_dtor)(void *) )
+void bds_stack_clear(struct bds_stack *stack)
 {
-	if ( elem_dtor != NULL ) {
-		while( !bds_stack_isempty( stack ) ) {
-			elem_dtor( bds_stack_pop( stack, NULL ) );
-		}		
-	}
-	stack->n_elem = 0;
+        if (stack->elem_dtor != NULL) {
+                while (!bds_stack_isempty(stack)) {
+                        stack->elem_dtor(bds_stack_pop(stack, NULL));
+                }
+        }
+        stack->n_elem = 0;
 }
 
-const void *bds_stack_topptr( const struct bds_stack *stack )
+const void *bds_stack_topptr(const struct bds_stack *stack)
 {
-	if( bds_stack_isempty( stack ) )
-		return NULL;
-	
-	return stack->v + bds_stack_top( stack ) * stack->elem_len;
+        if (bds_stack_isempty(stack))
+                return NULL;
+
+        return stack->v + bds_stack_top(stack) * stack->elem_len;
 }
 
-const void *bds_stack_lsearch( const struct bds_stack *stack, const void *key,
-			       int (*compar)( const void *a, const void *b ) )
+const void *bds_stack_lsearch(const struct bds_stack *stack, const void *key,
+                              int (*compar)(const void *a, const void *b))
 {
-	size_t i;
-	const void *v;
+        size_t i;
+        const void *v;
 
-	for( i=0; i<stack->n_elem; ++i ) {
-		v = stack->v + i*stack->elem_len;
-		if( compar( key, v ) == 0 ) return v;
-	}
-	return NULL;
+        for (i = 0; i < stack->n_elem; ++i) {
+                v = stack->v + i * stack->elem_len;
+                if (compar(key, v) == 0)
+                        return v;
+        }
+        return NULL;
 }
 
-const void *bds_stack_bsearch( const struct bds_stack *stack, const void *key,
-			       int (*compar)( const void *a, const void *b ) )
+const void *bds_stack_bsearch(const struct bds_stack *stack, const void *key,
+                              int (*compar)(const void *a, const void *b))
 {
-	return bsearch( key, stack->v, stack->n_elem, stack->elem_len, compar );
+        return bsearch(key, stack->v, stack->n_elem, stack->elem_len, compar);
 }
 
-const void *bds_stack_modify( struct bds_stack *stack, const void *key, const void *v,
-			      int (*compar)( const void *a, const void *b ) )
+const void *bds_stack_modify(struct bds_stack *stack, const void *key, const void *v,
+                             int (*compar)(const void *a, const void *b))
 {
-	void *v_key = (void *)bds_stack_lsearch( stack, key, compar );
+        void *v_key = (void *)bds_stack_lsearch(stack, key, compar);
 
-	if( v_key == NULL ) return NULL;
-	memcpy( v_key, v, sizeof(stack->elem_len) );
+        if (v_key == NULL)
+                return NULL;
+        memcpy(v_key, v, sizeof(stack->elem_len));
 
-	return v_key;
+        return v_key;
 }
 
-void bds_stack_qsort( struct bds_stack *stack,
-		      int (*compar)( const void *, const void *) )
+void bds_stack_qsort(struct bds_stack *stack, int (*compar)(const void *, const void *))
 {
-	qsort(stack->v, stack->n_elem, stack->elem_len, compar );	
+        qsort(stack->v, stack->n_elem, stack->elem_len, compar);
 }
