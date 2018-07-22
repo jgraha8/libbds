e7c0bc7c67e4ceca11229e9f9b74832ca60455bb
Jason Graham <jgraham@compukix.net>
2018-07-22 11:43:44 -0400

    Added min-heap support.

--- heap/heap.c
+++ heap/heap.c
@@ -11,6 +11,7 @@
 #define RIGHT(i) ((i + 1) << 1)
 #define PTR(heap, i) (heap->data + heap->elem_len * (i))
 #define MAX(a, b) (a > b ? a : b)
+#define MAX(a, b) (a > b ? a : b)
 
 #define BDS_HEAP_MAX (1 << 0)
 #define BDS_HEAP_MIN (1 << 1)
@@ -28,7 +29,6 @@ struct bds_heap {
         int (*compar)(const void *, const void *);
         void (*__heapify)(struct bds_heap *, int);
         void (*__reparent)(struct bds_heap *, int);
-        void (*__build)(struct bds_heap *);
         void (*__change_key)(struct bds_heap *, int, const void *);
 
         size_t elem_len;
@@ -70,10 +70,35 @@ static void max_heapify(struct bds_heap *heap, int node)
         }
 }
 
-static void max_build(struct bds_heap *heap)
+static void min_heapify(struct bds_heap *heap, int node)
 {
-        for (int n = heap->num_elem / 2 - 1; n >= 0; --n) {
-                heap->__heapify(heap, n);
+        int left  = LEFT(node);
+        int right = RIGHT(node);
+
+        if (left >= heap->num_elem && right >= heap->num_elem)
+                return;
+
+        int c        = INT_MAX;
+        int smallest = -1;
+
+        if (left < heap->num_elem) {
+                int __c = heap->compar(PTR(heap, left), PTR(heap, node));
+                if (__c < 0 && __c < c) {
+                        c        = __c;
+                        smallest = left;
+                }
+        }
+        if (right < heap->num_elem) {
+                int __c = heap->compar(PTR(heap, right), PTR(heap, node));
+                if (__c < 0 && __c < c) {
+                        c        = __c;
+                        smallest = right;
+                }
+        }
+
+        if (smallest > 0) {
+                memswap(PTR(heap, smallest), PTR(heap, node), heap->elem_len);
+                min_heapify(heap, smallest);
         }
 }
 
@@ -88,6 +113,19 @@ static void max_reparent(struct bds_heap *heap, int node)
         if (heap->compar(PTR(heap, parent), PTR(heap, node)) < 0) {
                 memswap(PTR(heap, parent), PTR(heap, node), heap->elem_len);
                 max_reparent(heap, parent);
+        }
+}
+
+static void min_reparent(struct bds_heap *heap, int node)
+{
+        if (node <= 0)
+                return;
+
+        int parent = PARENT(node);
+
+        if (heap->compar(PTR(heap, parent), PTR(heap, node)) > 0) {
+                memswap(PTR(heap, parent), PTR(heap, node), heap->elem_len);
+                min_reparent(heap, parent);
         }
 }
 
@@ -107,11 +145,43 @@ static void max_change_key(struct bds_heap *heap, int node, const void *val)
         }
 }
 
+static void min_change_key(struct bds_heap *heap, int node, const void *val)
+{
+        int c = heap->compar(val, PTR(heap, node));
+
+        if (c == 0)
+                return;
+
+        memcpy(PTR(heap, node), val, heap->elem_len);
+
+        if (c < 0) {
+                heap->__reparent(heap, node);
+        } else {
+                heap->__heapify(heap, node);
+        }
+}
+
 struct bds_heap *bds_heap_alloc(int heap_type, int num_alloc, size_t elem_len,
                                 int (*compar)(const void *, const void *), void *data)
 {
         struct bds_heap *heap = calloc(1, sizeof(*heap));
         assert(heap);
+
+        heap->heap_type = heap_type;
+
+        if (heap_type == BDS_HEAP_MAX) {
+                heap->__heapify    = max_heapify;
+                heap->__reparent   = max_reparent;
+                heap->__change_key = max_change_key;
+        } else if (heap_type == BDS_HEAP_MIN) {
+                heap->__heapify    = min_heapify;
+                heap->__reparent   = min_reparent;
+                heap->__change_key = min_change_key;
+        } else {
+                free(heap);
+                fprintf(stderr, "incorrect heap type\n");
+                exit(EXIT_FAILURE);
+        }
 
         heap->num_alloc = num_alloc;
         heap->elem_len  = elem_len;
@@ -121,21 +191,10 @@ struct bds_heap *bds_heap_alloc(int heap_type, int num_alloc, size_t elem_len,
                 heap->num_elem = num_alloc;
                 heap->data     = data;
         } else {
-                heap->data       = calloc(num_alloc, elem_len);
-                heap->data_owner = true;
+                heap->data = calloc(num_alloc, elem_len);
                 assert(heap->data);
-        }
 
-        heap->heap_type = heap_type;
-
-        if (heap_type == BDS_HEAP_MAX) {
-                heap->__heapify    = max_heapify;
-                heap->__reparent   = max_reparent;
-                heap->__build      = max_build;
-                heap->__change_key = max_change_key;
-        } else {
-                fprintf(stderr,"incorrect heap type\n");
-                exit(EXIT_FAILURE);
+                heap->data_owner = true;
         }
 
         return heap;
@@ -147,7 +206,8 @@ void bds_heap_free(struct bds_heap **heap)
                 return;
 
         if ((*heap)->data_owner) {
-                free((*heap)->data);
+                if ((*heap)->data)
+                        free((*heap)->data);
         }
         free(*heap);
         *heap = NULL;
@@ -155,8 +215,14 @@ void bds_heap_free(struct bds_heap **heap)
 
 void bds_heap_heapify(struct bds_heap *heap, int node) { heap->__heapify(heap, node); }
 void bds_heap_reparent(struct bds_heap *heap, int node) { heap->__reparent(heap, node); }
-void bds_heap_build(struct bds_heap *heap) { heap->__build(heap); }
 void bds_heap_change_key(struct bds_heap *heap, int node, const void *val) { heap->__change_key(heap, node, val); }
+
+void bds_heap_build(struct bds_heap *heap)
+{
+        for (int n = heap->num_elem / 2 - 1; n >= 0; --n) {
+                heap->__heapify(heap, n);
+        }
+}
 
 int bds_heap_pop(struct bds_heap *heap, void *val)
 {
@@ -221,19 +287,20 @@ int main(int argc, char **argv)
                 a[i] = rand() % 100;
         }
 
-	//struct bds_heap *heap = bds_heap_alloc(BDS_HEAP_MAX, N, sizeof(a[0]), compar, a);
+        // struct bds_heap *heap = bds_heap_alloc(BDS_HEAP_MAX, N, sizeof(a[0]), compar, a);
         struct bds_heap *heap = bds_heap_alloc(BDS_HEAP_MAX, N, sizeof(a[0]), compar, NULL);
-	memcpy(heap->data, &a[0], N*sizeof(a[0]));
-	heap->num_elem = N;
+        memcpy(heap->data, &a[0], N * sizeof(a[0]));
+        heap->num_elem = N;
 
         bds_heap_build(heap);
-	
+	memcpy(&a[0], heap->data, N * sizeof(a[0]));
+
         for (int i = 1; i < N; ++i) {
                 assert(a[PARENT(i)] >= a[i]);
         }
 
-	const int key = 101;
-	bds_heap_change_key(heap,N-1, &key); 
+        const int key = 101;
+        bds_heap_change_key(heap, N - 1, &key);
 
         int val;
         printf("a = { ");
